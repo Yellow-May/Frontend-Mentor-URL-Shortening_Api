5dac0c3e3aa4c088407f5a3fcb658c35bd3b6166
Yellow May <kwerep09@yahoo.com>
2021-03-23 13:23:08 +0100

    Cleanup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./sass/main.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className='attribution'>
+				Challenge by{" "}
+				<a
+					href='https://www.frontendmentor.io?ref=challenge'
+					target='_blank'
+					rel='noreferrer'>
+					Frontend Mentor
+				</a>
+				. Coded by{" "}
+				<a href='https://www.github.com/Yellow-May'>The Yellow May</a>.
+			</div>
+		</>
+	);
 }
 
 export default App;
